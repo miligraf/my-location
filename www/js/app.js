07f7468e223004795e535af39a41024d53e75f74
Milton Andrade <miligraf@gmail.com>
2016-09-19 17:31:46 -0400

    add google maps

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,29 @@
 
   var app = angular.module('starter', ['ionic', 'ngCordova']);
 
+  app.controller('LocationCtrl', function($scope, $cordovaGeolocation, $ionicPlatform) {
+
+    function showMap(coords) {
+      var mapOptions = {
+        center: {lat: coords.latitude, lng: coords.longitude},
+        zoom: 8
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    }
+
+    $ionicPlatform.ready(function() {
+      var posOptions = {timeout: 10000, enableHighAccuracy: true};
+      $cordovaGeolocation.getCurrentPosition(posOptions)
+        .then(function (position) {
+          $scope.coords = position.coords;
+          showMap(position.coords);
+        }, function(err) {
+          console.log('getCurrentPosition error: ' + angular.toJson(err));
+        });
+    });
+
+  });
+
   app.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
       if(window.cordova && window.cordova.plugins.Keyboard) {
